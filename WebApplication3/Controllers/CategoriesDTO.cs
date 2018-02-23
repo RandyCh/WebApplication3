ff04e91651bf25baea41eb12cdb08fd2d472fee6
RandyCheng <linusche@gmail.com>
2018-02-23 17:16:21 +0800

    加入專案檔案。

---
+++ WebApplication3/Controllers/CategoriesDTO.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class CategoriesDTO 
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+}
